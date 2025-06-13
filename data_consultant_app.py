f1e22abfc7896a7a9b019e85a5adcbd81475f50a
DRod0422 <davrod0422@gmail.com>
2025-06-12 23:41:08 -0500

    updated page title to AI Pocket Analyst

--- data_consultant_app.py
+++ data_consultant_app.py
@@ -18,7 +18,7 @@ GPT_MODEL = "gpt-4o"  # GPT-4.1 Mini/Nano
 
 # --- Page Config ---
 st.set_page_config(
-    page_title="Pocket Analyst",
+    page_title=" AI Pocket Analyst",
     layout="wide",
     page_icon="logo.png"
 )
