b21fe166b0f2d14f4d76e842c97ddd3fee2e97cf
DRod0422 <davrod0422@gmail.com>
2025-06-18 22:47:42 -0500

    updated line 477

--- data_consultant_app.py
+++ data_consultant_app.py
@@ -474,7 +474,7 @@ if uploaded_file:
                         from sklearn.model_selection import train_test_split
     
                         X = data_for_modeling[features]
-                        Y = data_for_modeling[target_col]
+                        y = data_for_modeling[target_col]
 
                         X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
     
